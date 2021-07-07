196af645ca1e0dd5d8f7140c3e309bfb40e3c293
admin <admin@192.168.0.4>
2021-07-07 09:54:06 +0530

    seres

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hii");
-		System.out.println("sir plz follow back me");
+		System.out.println("sir plz follow back m");
 	}
 
 }
