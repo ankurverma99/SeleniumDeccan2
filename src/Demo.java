7dd0d951b0c5cb2c9a96bb731da488f8d12a433a
SANKETC1999 <80206579+SANKETC1999@users.noreply.github.com>
2021-07-07 09:53:51 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,8 @@
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hii");
-		System.out.println("sir plz follow back me");
+		System.out.println("sir plz follow back me")
+			System.out.println("qspider");
 	}
 
 }
